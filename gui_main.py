b3660c32e231318164ef2a052c391caa678cecac
Jens Karlsson <jenscarlsson04@gmail.com>
2025-02-18 00:49:32 +0100

    temp

--- gui_main.py
+++ gui_main.py
@@ -2,17 +2,15 @@ import os
 
 os.environ["KIVY_NO_FILELOG"] = "1"  # eliminate file log
 from kivy.config import Config
-
-
+from pages.Dash2 import Dash2
+from pages.Inverter import Inverter
+from pages.TSCU import TSCU
+from pages.Dash import Dash
 Config.set("graphics", "width", "1024")
 Config.set("graphics", "height", "600")
 Config.set("graphics", "dpi", "96")
 
 from kivy.app import App
-from pages.Dash import Dash
-from pages.Inverter import Inverter
-from pages.Orion import Orion
-from pages.TSCU import TSCU
 from screen_switcher.screen_switcher import (
     CustomScreenSwitcher,
 )  # Renamed to CentralizedScreenSwitcher
@@ -24,10 +22,11 @@ class MainApp(App):
         sm = CustomScreenSwitcher()
 
         # Add screens to the screen manager
-        # sm.add_screen(Inverter(name="inverter"))
-        # sm.add_screen(Dash(name="Dashboard"))
+        sm.add_screen(Dash2(name="dashboard2"))
+        #sm.add_screen(Inverter(name="inverter"))
+        #sm.add_screen(Dash(name="Dashboard"))
         # sm.add_screen(Orion(name="orion"))
-        sm.add_screen(TSCU(name="tscu"))
+        #sm.add_screen(TSCU(name="tscu"))
 
         # Bind to detect screen changes and reset the clock update
         sm.bind(current=sm.on_current_screen)
