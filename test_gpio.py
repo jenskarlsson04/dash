81391262a72906df859a41b99d592db39c172a6a
Oliver Drevet <oliver.drevet@outlook.com>
2025-03-03 18:19:52 +0100

    gpio_test

---
+++ test_gpio.py
@@ -0,0 +1,31 @@
+from GPIO_reader import subscribe_gpio_pint, btn_screen
+import pigpio
+
+pin = 16
+
+def run():
+
+    print("GPIO triggered")
+
+pi = pigpio.pi()
+
+"""
+Configure pigpio
+"""
+# Set gpio as pull-down resistors
+pi.set_mode(pin, pigpio.PUD_DOWN)
+
+# Attach callbacks rising edge
+pi.callback(pin, pigpio.RISING_EDGE, run)
+
+# falling edge
+pi.callback(pin, pigpio.FALLING_EDGE, run)
+
+
+
+subscribe_gpio_pint(btn_screen, run)
+
+
+if __name__ == "__main__":
+    while True:
+        pass
