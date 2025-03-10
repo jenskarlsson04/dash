2d5b79c04b80e64de9401907c19cff8854238081
Oliver Drevet <oliver.drevet@outlook.com>
2025-03-10 19:49:26 +0100

    added file class

---
+++ FileSave/instaces.py
@@ -0,0 +1,8 @@
+import os
+from FileSave.file_save import DictSaver
+
+DATA_DIR = "data"
+
+STATS_FILENAME = os.path.join(DATA_DIR, "stats.json")
+PERSISTENT_FILENAME = os.path.join(DATA_DIR, "persistent_stats.json")
+
